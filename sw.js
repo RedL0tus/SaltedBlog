a5ce4eb2e88383c5667a535bbc93556d4ffb9c6b
Kay <i@v2bv.net>
2018-06-22 17:31:22 +0800

    service worker: Switch to Workbox

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 /*
- * Service Worker with sw-toolbox
+ * Service Worker with Workbox
  */
 
 "use strict";
 (function(){
-    const CONFIG = "config.json";
     let cacheVersion = "20180622";
     let staticImageCacheName = "image-" + cacheVersion;
     let staticAssetsCacheName = "assets-" + cacheVersion;
@@ -12,45 +11,66 @@
     let vendorCacheName = "vendor-" + cacheVersion;
     let maxEntries = 100;
 
-    // Import sw-toolbox
-    self.importScripts("https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbox.js");
-    self.toolbox.options.debug = false;
-    self.toolbox.options.networkTimeoutSeconds = 1;
+    /* Import Workbox */
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-    // Fetch config
-    fetch(CONFIG).then(config => config.json()).then(function(config) {
-        /* Image caching */
-        if (config.img != null) {
-            self.toolbox.router.get(config.img, self.toolbox.cacheFirst, {
-                origin: config.link,
-                cache: {
-                    name: staticImageCacheName,
-                    maxEntries: maxEntries
-                }
-            });
-        }
-        /* Image hosts other than host itself */
-        if (config.imgHosts.length > 0) {
-            for (let i = 0; i < config.imgHosts.length; i += 1) {
-                self.toolbox.router.get(config.imgHosts[i].path, self.toolbox.cacheFirst, {
-                    origin: config.imgHosts[i].host,
-                    maxEntries: maxEntries
-                });
-            }
-        }
+    if (workbox) {
+        /* Caching JS files */
+        workbox.routing.registerRoute(
+            new RegExp('.*\.js'),
+            workbox.strategies.staleWhileRevalidate({
+                cacheName: staticAssetsCacheName
+            })
+        );
+        /* Caching JSON metas and Markdown posts */
+        workbox.routing.registerRoute(
+            new RegExp('.*\.(json|md|markdown)'),
+            workbox.strategies.staleWhileRevalidate({
+                cacheName: contentCacheName
+            })
+        );
+        /* Caching HTML files */
+        workbox.routing.registerRoute(
+            new RegExp('.*\.(html|htm)'),
+            workbox.strategies.cacheFirst({
+                cacheName: staticAssetsCacheName,
+                plugins: [
+                    new workbox.expiration.Plugin({
+                        maxAgeSeconds: 7 * 24 * 60 * 60
+                    })
+                ]
+            })
+        );
+        /* Caching Images */
+        workbox.routing.registerRoute(
+            new RegExp('.*\.(?:png|jpg|jpeg|svg|gif|webp)'),
+            workbox.strategies.cacheFirst({
+                cacheName: staticImageCacheName,
+                plugins: [
+                    new workbox.expiration.Plugins({
+                        maxEntries: maxEntries,
+                        maxAgeSeconds: 7 * 24 * 60 * 60
+                    })
+                ]
+            })
+        );
+        /* Google Fonts */
+        workbox.routing.registerRoute(
+            new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+            workbox.strategies.cacheFirst({
+                cacheName: vendorCacheName,
+                plugins: [
+                    new workbox.expiration.Plugin({
+                        maxEntries: maxEntries,
+                    }),
+                    new workbox.cacheableResponse.Plugin({
+                        statuses: [0, 200]
+                    }),
+                ],
+            }),
+        );
 
-        /* Caching metas */
-        let parser = new URL(config.link);
-        if (config.meta != null) {
-            self.toolbox.router.get(parser.pathname + config.meta, self.toolbox.networkFirst, {
-                origin: parser.hostname,
-                maxEntries: maxEntries
-            });
-        }
-
-        self.toolbox.router.get(parser.pathname + CONFIG, self.toolbox.networkFirst, {
-            origin: parser.hostname,
-            maxEntries: maxEntries
-        });
-    });
+        /* Google Analytics */
+        // workbox.googleAnalytics.initialize();
+    }
 })();
