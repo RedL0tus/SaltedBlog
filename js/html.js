860373c72139eee25aee358b8db137c98590aaf3
Kay <i@v2bv.net>
2018-06-24 14:16:19 +0800

    Renderer: Add HTML direct output (dummy plugin)

---
+++ js/html.js
@@ -0,0 +1,12 @@
+"use strict";
+
+define([], function() {
+    return {
+        info: {
+            description: "HTML direct output"
+        },
+        render: function(text) {
+            return text
+        }
+    }
+});
